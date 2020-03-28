6f8e2ee1b6b692035d084ad48ed4e0c72df13991
Edwin Andrew <edwinandrew96@gmail.com>
2020-03-28 12:53:07 -0400

    Update MainProgram.java

--- part05-Part05_17.Money/src/main/java/MainProgram.java
+++ part05-Part05_17.Money/src/main/java/MainProgram.java
@@ -3,20 +3,19 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Money a = new Money(10, 0);
-        Money b = new Money(5, 0);
+        Money b = new Money(3, 50);
 
-        Money c = a.plus(b);
+        Money c = a.minus(b);
 
         System.out.println(a);  // 10.00e
-        System.out.println(b);  // 5.00e
-        System.out.println(c);  // 15.00e
+        System.out.println(b);  // 3.50e
+        System.out.println(c);  // 6.50e
 
-        a = a.plus(c);          // NB: a new Money object is created, and is placed "at the end of the strand connected to a"
-        //  the old 10 euros at the end of the strand disappears and the Java garbage collector takes care of it
+        c = c.minus(a);       // NB: a new Money object is created, and is placed "at the end of the strand connected to c"
+//  the old 6.5 euros at the end of the strand disappears and the Java garbage collector takes care of it
 
-        System.out.println(a);  // 25.00e
-        System.out.println(b);  // 5.00e
-        System.out.println(c);  // 15.00e
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 3.50e
+        System.out.println(c);  // 0.00e
     }
 }
-    
